10961946c959494a672786b15d3140ffc11b9432
shihabsaleem <shihabchikkushihab@gmail.com>
2024-07-05 00:59:44 +0530

    navbar

--- PDS-APP/src/app/app.module.ts
+++ PDS-APP/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
